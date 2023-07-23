696cd4f9673efe610fcc18fe3f6f6d9a3da3486a
Jang-Sohyun <111950769+Jang-Sohyun@users.noreply.github.com>
2023-07-23 15:23:53 +0900

    expo start error

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,8 @@
 module.exports = function(api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: ['babel-preset-expo', 'next/babel', '@babel/preset-env', '@babel/preset-react'],
+    plugins: ['@babel/plugin-proposal-class-properties'],
+    ignore: ['node_modules/next/dist/compiled/gzip-size/index.js'],
   };
 };
